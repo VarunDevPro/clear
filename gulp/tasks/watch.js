b63bd44be5f1aa421f8fa8b1611fdffdaa89a546
Varun <28918411+VarunDevPro@users.noreply.github.com>
2019-04-27 08:58:25 +0530

    first commit

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,36 @@
+var gulp       = require('gulp'),
+	watch        = require('gulp-watch'),
+	browserSync  = require('browser-sync');
+
+gulp.task('watch',function() {
+	browserSync.init({
+		notify:false,
+		server: {
+			baseDir:"app",
+		}
+	});
+
+	watch('./app/index.html', function() {
+		browserSync.reload();
+	});
+
+  watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('cssInject');
+	});
+
+  watch('./app/assets/scripts/**/*.js', function() {
+		gulp.start('scriptsRefresh');
+	});
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function () {
+	browserSync.reload();
+})
+gulp.task('cssInject',['styles'],function () {
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});
+
+gulp.task('default',function () {
+	console.log("default task");
+});
